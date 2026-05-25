744f4fb10cba9be63685f7c382bc648091aca44b
DevOps <devops@algobench>
2026-05-25 15:06:39 +0000

    perf: self-host IBM Plex Mono and Cormorant Garamond fonts
    Remove Google Fonts external dependency (2 roundtrips to googleapis.com
    + gstatic.com). Bundle all 40 woff2 files locally under priv/static/fonts/
    and serve via /fonts.css. Eliminates 3rd-party DNS lookup on every page load.

--- lib/agent_mmo_web.ex
+++ lib/agent_mmo_web.ex
@@ -17,7 +17,7 @@ defmodule AgentMmoWeb do
   those modules here.
   """
 
-  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt)
+  def static_paths, do: ~w(assets fonts fonts.css images favicon.ico robots.txt)
 
   def router do
     quote do
